56659e414d6ef5be1271f61685a0c6050d9978d3
Ben Bradshaw <ben.bradshaw@sharesies.co.nz>
2023-12-03 11:46:13 +1300

    Day 1, Part 2

--- 2023/day1/day1.py
+++ 2023/day1/day1.py
@@ -1,10 +1,17 @@
 import re
+from word2number import w2n
 
 total = 0
 
+matches = ['1', '2', '3', '4', '5', '6', '7', '8', '9', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine']
+
 for line in open('input.txt'):
-    numbers = re.findall(r'\d', line)
-    line_number = f"{numbers[0]}{numbers[-1]}"
+    #numbers = re.findall(r'\d', line)
+    numbers = re.findall(r"(?=("+'|'.join(matches)+r"))", line)
+
+    num1 = w2n.word_to_num(numbers[0])
+    num2 = w2n.word_to_num(numbers[-1])
+    line_number = f"{num1}{num2}"
 
     total += int(line_number)
 
